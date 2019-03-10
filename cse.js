d00119c510840b8016c62eb268339d14f704504b
permata19 <43102286+permata19@users.noreply.github.com>
2019-03-10 16:03:18 +0700

    Create cse.js

---
+++ cse.js
@@ -0,0 +1,9 @@
+(function() {
+    var cx = 'partner-pub-7576453856298941:1263593630';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
